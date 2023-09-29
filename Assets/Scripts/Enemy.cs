5d2c2a725dcf73e9c0b67414237ca772882393ab
Ethan M <115674465+ethanmcgimpsey@users.noreply.github.com>
2023-09-29 14:31:21 +1000

    Update #2

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public enum AIState
+    {
+        Walking,
+        Die
+    }
+    public AIState state;
+    public float curHealth, maxHealth, moveSpeed;
+    public int curWaypoint, difficulty;
+    public float minDistance = 2f;
+    public bool isDead, dead;
+
+    [Space(5), Header("Base References")]
+    public GameObject self;
+    public Transform waypointParent;
+    protected Transform[] waypoints;
+    public NavMeshAgent agent;
+    public GameObject healthCanvas;
+    public Image healthBar;
+    public Animator anim;
+    public Transform targetPoint;
+
+    private void OnDrawGizmos()
+    {
+        // If waypoints isn't assigned
+        if (waypoints == null)
+            return;
+
+        // If there are waypoints in the array
+        if(waypoints.Length > 0)
+        {
+            // Get current waypoint
+            Transform waypoint = waypoints[curWaypoint];
+            // If waypoint exists
+            if (waypoint)
+            {
+                // Draw it!
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireSphere(waypoint.position, minDistance);
+            }
+        }
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position , .5f);
+        Gizmos.DrawSphere(agent.destination, .5f);
+
+    }
+    public void Start()
+    {
+        waypoints = waypointParent.GetComponentsInChildren<Transform>();
+        curWaypoint = 1;
+        agent.speed = moveSpeed;
+        anim = self.GetComponent<Animator>();
+        Walking();
+        anim.SetBool("Walking", false);
+        anim.SetBool("Die", false);
+
+    }
+    private void Update()
+    {
+        anim.SetBool("Walking", false);
+        anim.SetBool("Die", false);
+
+        Walking();
+        Die();
+    }
+    void LateUpdate()
+    {
+        if (healthBar.fillAmount < 1 && healthBar.fillAmount > 0)
+        {
+            healthCanvas.SetActive(true);
+            healthCanvas.transform.LookAt(Camera.main.transform);
+            healthCanvas.transform.Rotate(0, 180, 0);
+        }
+        else if (healthCanvas.activeSelf == true)
+        {
+            healthCanvas.SetActive(false);
+        }
+    }
+
+    public void Walking()
+    {
+        if (!isDead)
+        {
+            // DO NOT CONTINUE IF NO WAYPOINTS
+            if (waypoints.Length == 0)
+            {
+                return;
+            }
+            anim.SetBool("Walking", true);
+            // Follow waypoints
+            // Set agent to target
+            agent.destination = waypoints[curWaypoint].position;
+            // Are we at the waypoint?
+            float distance = Vector3.Distance(transform.position, agent.destination);
+            if (distance < minDistance)
+            {
+                if (curWaypoint < waypoints.Length - 1)
+                {
+                    // If so go to next waypoint
+                    curWaypoint++;
+                    curWaypoint = Random.Range(2, 4);
+                }
+                else
+                {
+                    // If at the end of patrol go to start
+                    // curWaypoint = 3;
+                    Destroy(gameObject);
+                    PlayerStats.Lives -= 3;
+                }
+                // If so go to next waypoint
+            }
+
+        }
+    }
+
+    public void Die()
+    {
+        // If we are alive
+        if (curHealth > 0)
+        {
+            // Don't run this
+            return;
+        }
+        // else we are dead so run this
+        if (isDead)
+        {
+            state = AIState.Die;
+            if (!dead)
+            {
+                anim.SetTrigger("Die");
+                dead = true;
+                PlayerStats.Money += 14;
+                PlayerStats.Score += 7;
+            }
+            agent.destination = self.transform.position;
+            // Drop Loot
+        }
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,198 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using System.Xml.Serialization;
+using System.IO;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public enum SpawnState
+    {
+        Spawning,
+        Waiting,
+        Counting
+    };
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public int index;
+        [XmlIgnore] public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    public int nextWave = 0;
+    public Transform waypointParent;
+    public GameObject completeLevelUI, finalWaveUI;
+    public Text waveTextRound;
+    // public Text score;
+    public AudioSource nextWaveAudio;
+    public AudioSource gameOverAudio;
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+    public SpawnState state = SpawnState.Counting;
+
+    [Header("Saving")]
+    public string fileName = "GameSave";
+
+    private string fullPath;
+
+    private float searchCountdown = 1f;
+
+
+    void Start()
+    {
+        /* fullPath = Application.persistentDataPath + "/" + fileName + ".xml";
+
+        if(File.Exists(fullPath))
+        {
+            Wave currentWave = Load(fullPath);
+            SetWave(currentWave.index);
+        }*/
+
+        Enemy enemy = GetComponent<Enemy>();
+        waveCountdown = timeBetweenWaves;
+        finalWaveUI.SetActive(false);
+    }
+
+    void SetWave(int waveIndex)
+    {
+        nextWave = waveIndex;
+        nextWaveAudio.Play();
+        state = SpawnState.Counting;
+        waveCountdown = timeBetweenWaves;
+        waveTextRound.text = "Wave: " + (nextWave + 1).ToString();
+    }
+
+    void Update()
+    {
+        // score.text = PlayerStats.Score.ToString() + " :Points";
+        if (state == SpawnState.Waiting)
+        {
+            if (EnemyIsAlive())
+            {
+                return;
+            }
+            else
+            {
+                Debug.Log("Wave Completed");
+                nextWave++;
+                SetWave(nextWave);
+                waves[nextWave].index = nextWave;
+                // Save(fullPath, waves[nextWave]);
+            }
+        }
+
+        if (waveCountdown <= 0)
+        {
+            if(state != SpawnState.Spawning)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+
+        /*
+        switch (nextWave)
+        {
+            case 4:
+                finalWaveUI.gameObject.SetActive(true);
+                if (state == SpawnState.Spawning)
+                {
+                    finalWaveUI.gameObject.SetActive(false);
+                }
+                break;
+            case 5:
+                completeLevelUI.SetActive(true);
+                break;
+        }
+        */
+
+        if (nextWave == 4)
+        {
+            finalWaveUI.gameObject.SetActive(true);
+            if (state == SpawnState.Spawning)
+            {
+                finalWaveUI.gameObject.SetActive(false);
+            }
+        }
+
+        if (nextWave == 5)
+        {
+            completeLevelUI.SetActive(true);
+        }
+
+        if(PlayerStats.Lives <= 0)
+        {
+            if (!gameOverAudio.isPlaying)
+            {
+                Debug.Log("Play this audio source");
+                gameOverAudio.Play();
+            }
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if(searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        Debug.Log("Spawning Enemy" + _wave.name);
+        state = SpawnState.Spawning;
+        // Spawn
+        for (int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f/_wave.rate);
+        }
+
+        state = SpawnState.Waiting;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        Debug.Log("Spawning Enemy: " + _enemy.name);
+        Transform clone = Instantiate(_enemy, transform.position, transform.rotation);
+        Enemy enemy = clone.GetComponent<Enemy>();
+        enemy.waypointParent = waypointParent;
+    }
+
+
+    /* public void Save(string path, Wave wave)
+    {
+        var serializer = new XmlSerializer(typeof(Wave));
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+            serializer.Serialize(stream, wave);
+        }
+    }*/
+
+    public static Wave Load(string path)
+    {
+        var serializer = new XmlSerializer(typeof(Wave));
+        using (var stream = new FileStream(path, FileMode.Open))
+        {
+            return serializer.Deserialize(stream) as Wave;
+        }
+    }
+}
